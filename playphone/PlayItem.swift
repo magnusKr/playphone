3ee4d75d992f8b98b4480d92d57e2d9d81862132
magnusKr <magnuskreppe@gmail.com>
2016-11-13 15:49:22 +0100

    Add classes

---
+++ playphone/PlayItem.swift
@@ -0,0 +1,9 @@
+//
+//  PlayItem.swift
+//  playphone
+//
+//  Created by Magnus Kraepelien on 13/11/16.
+//  Copyright © 2016 playphone. All rights reserved.
+//
+
+import Foundation
---
+++ playphone/PlayItemsService.swift
@@ -0,0 +1,9 @@
+//
+//  PlayItemsService.swift
+//  playphone
+//
+//  Created by Magnus Kraepelien on 13/11/16.
+//  Copyright © 2016 playphone. All rights reserved.
+//
+
+import Foundation
